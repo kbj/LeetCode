27b17fd353f67e32020d3a1e5e4fb26486e8d8f1
Weey <piaoxuewuyin@gmail.com>
2020-04-12 14:48:56 +0800

    update

---
+++ src/me/weey/leetcode/tree/TreeNode.java
@@ -0,0 +1,15 @@
+package me.weey.leetcode.tree;
+
+/**
+ * @author Weey
+ * @program: Leetcode
+ * @package me.weey.leetcode.tree
+ * @description: TODO
+ * @date 2020-04-12 14:13
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
