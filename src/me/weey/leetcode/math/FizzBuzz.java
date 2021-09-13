cdec13c71a53bd9f98c97f58708653b7983f626f
Weey <piaoxuewuyin@gmail.com>
2021-09-13 21:48:48 +0800

    update

--- src/me/weey/leetcode/math/FizzBuzz.java
+++ src/me/weey/leetcode/math/FizzBuzz.java
@@ -15,7 +15,7 @@ public class FizzBuzz {
       fizzBuzz(15);
     }
 
-    public List<String> fizzBuzz(int n) {
+    public static List<String> fizzBuzz(int n) {
         List<String> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             int num = i + 1;
---
+++ src/me/weey/leetcode/math/IsPowerOfThree.java
@@ -0,0 +1,19 @@
+package me.weey.leetcode.math;
+
+/**
+ * 给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。
+ *
+ * 整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3x
+ *
+ * @author Weey
+ * @date 2021-09-13 21:31
+ */
+public class IsPowerOfThree {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(243));
+    }
+    public static boolean isPowerOfThree(int n) {
+        // 　logx(y) =loge(y) / loge(x)
+        return (Math.log(n) / Math.log(3) + Math.E) % 1 <= 2 * Math.E;
+    }
+}
